b3b1d977c735d0e1ad84c04867f0d300eedac522
Sanaxen <bccatfalcon@gmail.com>
2021-08-31 07:41:23 +0900

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 copy main.cpp stylegancpp /v /y
 
-echo %~dp0\application\bin > .\application\bin\stylegan_path.txt
-echo %~dp0\application\bin > .\styleGANapp\styleGANapp\bin\Debug\stylegan_path.txt
-echo %~dp0\application\bin > .\styleGANapp\styleGANapp\bin\Release\stylegan_path.txt
+echo %~dp0\application\bin> .\application\bin\stylegan_path.txt
+echo %~dp0\application\bin> .\styleGANapp\styleGANapp\bin\Debug\stylegan_path.txt
+echo %~dp0\application\bin> .\styleGANapp\styleGANapp\bin\Release\stylegan_path.txt
